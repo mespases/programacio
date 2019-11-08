f0a5bf817a8f02a572d0d5b63c07d47801953a41
Miguel <45602999+mespases@users.noreply.github.com>
2019-11-08 10:43:42 +0100

    Add files via upload

---
+++ usuario.py
@@ -0,0 +1,55 @@
+from flask_login import UserMixin
+from pymongo import MongoClient
+import json
+
+
+class User(UserMixin):
+    """ Clase Users con herencia de UserMixin, la clase esta preparada para usarse
+        con mongodb. Clase preparada para login de usuarios mediante flask-WTForms"""
+
+    # Conexion con mongo
+    cliente = MongoClient('localHost', 27017)
+    db = cliente.Practica
+    collection_usuarios = db['Usuarios']
+
+    email = []
+    password = []
+    role = []
+
+    def __init__(self, email="", password="", role=""):
+        self.loadUsers()
+        if email != "" and password != "":
+            self.email.append(email)
+            self.password.append(password)
+            self.role.append(role)
+            self.saveUser()
+
+
+    def loadUsers(self):
+        """ Carga los usuarios de mongodb """
+        for usuario in self.collection_usuarios.find():
+            self.email.append(usuario.get("Email"))
+            self.password.append(usuario.get("Password"))
+            self.role.append(usuario.get("Role"))
+
+
+    def saveUser(self):
+        """ Guarda el usuario en mongodb """
+        datos_json = {
+            "Email": self.email[-1],
+            "Password": self.password[-1],
+            "Role": self.role[-1]
+        }
+
+        datos_json = json.dumps(datos_json)
+        self.collection_usuarios.insert_one(json.loads(datos_json))
+
+
+    def get_id(self):
+        return self.email
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
